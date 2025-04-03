545000f6aae87d57ed13cf70af9ff86ecd30038d
CarloshDevBR <carlos.sortudox1@gmail.com>
2025-04-02 23:39:58 -0300

    chore: start koin

---
+++ app/src/main/java/com/example/chatapp/ChatAppApplication.kt
@@ -0,0 +1,22 @@
+package com.example.chatapp
+
+import android.app.Application
+import com.example.chatapp.di.ApplicationModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class ChatAppApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        start()
+    }
+
+    private fun start() {
+        startKoin {
+            androidContext(this@ChatAppApplication)
+            ApplicationModules().load()
+            androidLogger()
+        }
+    }
+}
